fce513c1a91a5ece1a9345e95e8f49bedab493b4
Tanya Zhuge <tanyaxz15@gmail.com>
2021-07-30 15:58:03 -0700

    Create house obj

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+  location: "Bay Area",
+  color: "red",
+  yearBuilt: 1955,
+  yearRemodeled: 2000,
+  isNew: false,
+  hasABackyard: true,
+  needsUpgradesIn: ["Kitchen", "Bathroom"],
+  ageOfKidsInHouse: [2,2]    
+}
