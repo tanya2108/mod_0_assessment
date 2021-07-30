02aad8a2cc5ff9d21e0fbc39a7bc37b5b0ba3e92
Tanya Zhuge <tanyaxz15@gmail.com>
2021-07-30 15:31:09 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var animal = "wolf";
+var isCuddly = true;
+animal = "cat";
+var num8 = 9;
+var num3 = 21;
+var quotient = num3/num8;
+var favoriteFilms = ["Coco", "Sound of Music", "Shawshank Redemption"];
+favoriteFilms.shift()
+console.log(animal.length)
